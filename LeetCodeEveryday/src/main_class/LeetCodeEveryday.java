a5c6d43bd7a75f6c54eac8f49e8fe947f2dcd36e
chenmin <chenmin@nlclassifieds.com>
2016-10-03 12:23:48 -0230

    push

---
+++ LeetCodeEveryday/src/main_class/LeetCodeEveryday.java
@@ -0,0 +1,13 @@
+package main_class;
+
+public class LeetCodeEveryday {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s = "this is a test for reverse words in String";
+		char[] arr = s.toCharArray();
+		StringAndArrayUtil.reverseWords(arr);
+		System.out.println(arr);
+	}
+
+}
---
+++ LeetCodeEveryday/src/main_class/StringAndArrayUtil.java
@@ -0,0 +1,39 @@
+package main_class;
+
+/**
+ * @author chenmin
+ * 
+ *
+ */
+public class StringAndArrayUtil {
+	
+	/**
+	 * Given an input string, reverse the string word by word. A word is defined as a sequence of non-space characters.
+	 * The input string does not contain leading or trailing spaces and the words are always separated by a single space.
+	 * For example,Given s = "the sky is blue", return "blue is sky the".
+	 */
+	public static void reverseWords(char[] arr){
+//		char[] arr = s.toCharArray();
+		int j = 0;
+		for (int i = 0; i < arr.length;i++){   // reverse each word in string, except last one
+			if (arr[i]==' '){
+				reverseString(arr, j , i-1);
+				j = i+1;
+				}
+		}
+		reverseString(arr, j, arr.length-1);    // reverse last word
+		reverseString(arr, 0 , arr.length-1);	//reverse whole string
+		
+	}
+	
+	public static void reverseString(char[] arr, int i , int j){
+		while (i<j){
+			char temp = arr[i];
+			arr[i]=arr[j];
+			arr[j]=temp;
+			i++;
+			j--;
+		}
+	}
+	
+}
